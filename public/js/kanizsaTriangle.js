4d584e3536e04594d2bbf9ed43029e868151eb61
B1tF8er <ofeth@hotmail.com>
2019-02-08 18:08:13 -0700

    Add kanizsa triangle optical illusion

---
+++ public/js/kanizsaTriangle.js
@@ -0,0 +1,72 @@
+function kanizsaTriangle() {
+  let canvasElement = document.getElementById("kanizsaTriangle");
+  let context = canvasElement.getContext("2d");
+
+  firstCircle(context);
+  secondCircle(context);
+  thirdCircle(context);
+  firstTriangle(context);
+  secondTriangle(context);
+}
+
+function firstCircle(context) {
+    context.beginPath();
+    context.arc(80, 60, 30, 0, 2 * Math.PI);
+    context.stroke();
+    context.closePath();
+
+    context.fillStyle = "#000";
+    context.fill();
+}
+
+function secondCircle(context) {
+    context.beginPath();
+    context.arc(220, 60, 30, 0, 2 * Math.PI);
+    context.stroke();
+    context.closePath();
+
+    context.fillStyle = "#000";
+    context.fill();
+}
+
+function thirdCircle(context) {
+    context.beginPath();
+    context.arc(150, 190, 30, 0, 2 * Math.PI);
+    context.stroke();
+    context.closePath();
+
+    context.fillStyle = "#000";
+    context.fill();
+}
+
+function firstTriangle(context) {
+    context.beginPath();
+    context.moveTo(150, 20);
+    context.lineTo(75, 150);
+    context.lineTo(225, 150);
+    context.closePath();
+    // the outline
+    context.lineWidth = 5;
+    context.strokeStyle = "#000";
+    context.stroke();
+    // the fill color
+    context.fillStyle = "#FFF";
+    context.fill();
+}
+
+function secondTriangle(context) {
+    context.beginPath();
+    context.moveTo(150, 180);
+    context.lineTo(75, 60);
+    context.lineTo(225, 60);
+    context.closePath();
+    // the outline
+    context.lineWidth = 5;
+    context.strokeStyle = "#FFF";
+    context.stroke();
+    // the fill color
+    context.fillStyle = "#FFF";
+    context.fill();
+}
+
+export default kanizsaTriangle;
--- public/js/opticalIllusions.js
+++ public/js/opticalIllusions.js
@@ -1,12 +1,14 @@
 import zollner from './zollner.js';
 import cafeWall from './cafeWall.js';
 import blinkingDots from './blinkingDots.js';
+import kanizsaTriangle from './kanizsaTriangle.js';
 
 class opticalIllusions {
   static display() {
     zollner();
     cafeWall();
     blinkingDots();
+    kanizsaTriangle();
   }
 }
 
