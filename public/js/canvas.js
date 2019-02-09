53c519336c038b3509fe7210a959e2a8fd099c8e
B1tF8er <ofeth@hotmail.com>
2019-02-09 15:09:48 -0700

    Add canvas class to abstract the drawing of shapes

---
+++ public/js/canvas.js
@@ -0,0 +1,67 @@
+export default class canvas {
+  get width() {
+    return this.canvas.width;
+  }
+
+  get height() {
+    return this.canvas.height;
+  }
+
+  constructor(contextId) {
+    this.canvas = document.getElementById(contextId);
+    if (this.canvas.getContext) {
+      this.context = this.canvas.getContext("2d");
+    }
+  }
+
+  addRectangle(dimensions) {
+    this.context.beginPath();
+    this.context.fillRect(
+      dimensions.position.x,
+      dimensions.position.y,
+      dimensions.width,
+      dimensions.height
+    );
+    this.context.stroke();
+    this.context.closePath();
+  }
+
+  addCircle(dimensions, style) {
+    this.context.beginPath();
+    this.context.arc(
+      dimensions.position.centerX,
+      dimensions.position.centerY,
+      dimensions.radius,
+      dimensions.startAngle * Math.PI,
+      dimensions.endAngle * Math.PI,
+      dimensions.counterClockwise
+    );
+    this.context.stroke();
+    this.context.closePath();
+
+    // the fill color
+    this.context.fillStyle = style.fillStyle;
+    this.context.fill();
+
+    // the outline
+    this.context.lineWidth = style.lineWidth;
+    this.context.strokeStyle = style.strokeStyle;
+  }
+
+  addTriangle(dimensions, style) {
+    this.context.beginPath();
+    this.context.moveTo(dimensions.startPosition.x, dimensions.startPosition.y);
+    this.context.lineTo(dimensions.leftPosition.x, dimensions.leftPosition.y);
+    this.context.lineTo(dimensions.rightPosition.x, dimensions.rightPosition.y);
+    this.context.closePath();
+
+    // the fill color
+    this.context.fillStyle = style.fillStyle;
+    this.context.fill();
+
+    // the outline
+    this.context.lineWidth = style.lineWidth;
+    this.context.strokeStyle = style.strokeStyle;
+    this.context.stroke();
+  }
+}
