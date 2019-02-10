20f1ff8eccb89c51e958f0543be934a88b9478fa
B1tF8er <ofeth@hotmail.com>
2019-02-10 15:45:52 -0700

    Add style to muller lyer module

--- public/js/mullerLyer.js
+++ public/js/mullerLyer.js
@@ -3,30 +3,38 @@ import point from "./point.js";
 import size from "./size.js";
 import line from "./line.js";
 import rectangle from "./rectangle.js";
+import style from "./style.js";
+import constants from "./constants.js";
 
 function mullerLyer() {
   const mullerLyerCanvas = new canvas("muller-lyer");
 
-  leftRectangle(mullerLyerCanvas);
-  rightRectangle(mullerLyerCanvas);
+  addLeftRectangle(mullerLyerCanvas);
+  addRightRectangle(mullerLyerCanvas);
 }
 
-function leftRectangle(mullerLyerCanvas) {
+function addLeftRectangle(mullerLyerCanvas) {
   mullerLyerCanvas.addRectangle(new rectangle(new point(50, 40), new size(80, 150)));
+  mullerLyerCanvas.addStyle(new style(constants.Transparent, constants.Black, 3));  
+
   mullerLyerCanvas.addLine(new line(new point(50, 60), new point(90, 50)));
   mullerLyerCanvas.addLine(new line(new point(90, 50), new point(130, 60)));
   mullerLyerCanvas.addLine(new line(new point(90, 50), new point(90, 180)));
   mullerLyerCanvas.addLine(new line(new point(50, 170), new point(90, 180)));
   mullerLyerCanvas.addLine(new line(new point(90, 180), new point(130, 170)));
+  mullerLyerCanvas.addStyle(new style(constants.Black, constants.Black, 3));
 }
 
-function rightRectangle(mullerLyerCanvas) {
+function addRightRectangle(mullerLyerCanvas) {
   mullerLyerCanvas.addRectangle(new rectangle(new point(150, 40), new size(80, 150)));
+  mullerLyerCanvas.addStyle(new style(constants.Transparent, constants.Black, 3));
+
   mullerLyerCanvas.addLine(new line(new point(150, 40), new point(190, 50)));
   mullerLyerCanvas.addLine(new line(new point(190, 50), new point(230, 40)));
   mullerLyerCanvas.addLine(new line(new point(190, 50), new point(190, 180)));
   mullerLyerCanvas.addLine(new line(new point(150, 190), new point(190, 180)));
   mullerLyerCanvas.addLine(new line(new point(190, 180), new point(230, 190)));
+  mullerLyerCanvas.addStyle(new style(constants.Black, constants.Black, 3));
 }
 
 export default mullerLyer;
