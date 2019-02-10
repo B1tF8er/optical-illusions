c635358c345325ae9c7e24a04ea7c4a3f7762b60
B1tF8er <ofeth@hotmail.com>
2019-02-10 11:55:42 -0700

    Add constants class

---
+++ public/js/constants.js
@@ -0,0 +1,17 @@
+export default class constants {
+    static get White() {
+        return "#FFF";
+    }
+
+    static get Black() {
+        return "#000"
+    }
+
+    static get Clockwise() {
+        return false;
+    }
+
+    static get CounterClockwise() {
+        return true;
+    }
+}
