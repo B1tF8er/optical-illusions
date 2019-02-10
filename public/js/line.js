d4f146c5cce2bbe7089a9dfa40bfeb16a14ef3ca
B1tF8er <ofeth@hotmail.com>
2019-02-09 20:23:40 -0700

    Fix style

--- public/js/line.js
+++ public/js/line.js
@@ -1,14 +1,14 @@
 export default class line {
-    get from() {
-        return this.__from__;
-    }
+  get from() {
+    return this.__from__;
+  }
 
-    get to() {
-        return this.__to__;
-    }
+  get to() {
+    return this.__to__;
+  }
 
-    constructor(from, to) {
-        this.__from__ = from;
-        this.__to__ = to;
-    }
+  constructor(from, to) {
+    this.__from__ = from;
+    this.__to__ = to;
+  }
 }
